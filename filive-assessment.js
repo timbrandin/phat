c09e28e722d9615ec315bcbbbc7668680ff9be87
Robin Stroobants <vrt@webbit.be>
2015-06-03 19:34:08 -0700

    picture upgrade

--- filive-assessment.js
+++ filive-assessment.js
@@ -1,12 +1,11 @@
 if (Meteor.isClient) {
-  Template.assessment.onRendered(function(){
-    $(document).ready(function(){
-
-        $(".buddy").on("swiperight",function(){
-          $(this).addClass('rotate-left').delay(700).fadeOut(1);
-          $('.buddy').find('.status').remove();
-
-          $(this).append('<div class="status like">Like!</div>');
+	Template.assessment.onRendered(function(){
+		$(document).ready(function(){
+			$(".buddy").on("swiperight",function(){
+				$(this).addClass('rotate-left').delay(700).fadeOut(1);
+				$('.buddy').find('.status').remove();
+				
+				$(this).append('<div class="status like">Like!</div>');
           if ( $(this).is(':last-child') ) {
             $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
            } else {
