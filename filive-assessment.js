a4f239693ed90347ba59754e1e8d095d15fea663
Robin Stroobants <vrt@webbit.be>
2015-06-03 22:10:19 -0700

    slider

--- filive-assessment.js
+++ filive-assessment.js
@@ -49,20 +49,7 @@ if (Meteor.isClient) {
              }
           });
 
-         $(".buddy").on("swipeleft",function(){
-           console.log('ME - ' + slide_id);
-           
-          $(this).addClass('rotate-right').delay(700).fadeOut(1);
-          $('.buddy').find('.status').remove();
-          $(this).append('<div class="status dislike">NOT ME!</div>');
-
-          if ( $(this).is(':last-child') ) {
-           $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
-            alert('Na-na!');
-           } else {
-              $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
-          }
-        });
+        
          
       }
   });
@@ -80,17 +67,30 @@ if (Meteor.isClient) {
   Template.assessment.events({
     "swipeleft .buddy": function (e, instance) {
       var slide_id = this.id;
-      console.log();
       var slide_position = this.position;
       console.log('Not ME - ' + slide_id);
       $('#id-' + this.id).addClass('rotate-right').delay(700).fadeOut(1);
       $('.buddy').find('.status').remove();
-      $(e.target).append('<div class="status dislike">NOT ME!</div>');
+      $('#id-' + this.id).append('<div class="status dislike">NOT ME!</div>');
       if ( $('#id-' + this.id).is(':last-child')) {
         $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
         alert('Na-na!');
       } else {
-        $(e.target).next().removeClass('rotate-left rotate-right').fadeIn(400);
+        $('#id-' + this.id).next().removeClass('rotate-left rotate-right').fadeIn(400);
+      }
+    },
+    "swiperight .buddy": function (e, instance) {
+      var slide_id = this.id;
+      var slide_position = this.position;
+      console.log('ME - ' + slide_id);
+      $('#id-' + this.id).addClass('rotate-left').delay(700).fadeOut(1);
+      $('.buddy').find('.status').remove();
+      $('#id-' + this.id).append('<div class="status dislike">NOT ME!</div>');
+      if ( $('#id-' + this.id).is(':last-child')) {
+        $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
+        alert('Na-na!');
+      } else {
+        $('#id-' + this.id).next().removeClass('rotate-left rotate-right').fadeIn(400);
       }
     }
   })
