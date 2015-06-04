518b0c32ba24afe9247499d047114d4bf5071c4e
Tim Brandin <tim.brandin@gmail.com>
2015-06-03 19:39:51 -0700

    Added some traitify code.

--- filive-assessment.js
+++ filive-assessment.js
@@ -28,5 +28,17 @@ if (Meteor.isClient) {
       });
 
     });
+
+    // Adding assessments
+    Traitify.setPublicKey("8asdf8sda-f98as-df8ads-fadsf"); // Example Public Key
+    Traitify.setHost("api-sandbox.traitify.com"); // Example host url (Defaults to api.traitify.com)
+    Traitify.setVersion("v1"); // Example Version
+    var assessmentId = "34aeraw23-3a43a32-234a34as42"; // Example Assessment id
+
+    traitify = Traitify.ui.load(assessmentId, ".slide-deck", {
+        results: {target: ".results"},
+        personalityTypes: {target: ".personality-types"},
+        personalityTraits: {target: ".personality-traits"}
+    }); // Example selector for widget target
   });
 }
